ee4dad43424e34390a7dc505540c2926286f9dd4
huecheng <huecheng@tencent.com>
2024-04-11 16:59:09 +0800

    feat: download pretrain model from cos

---
+++ tikit_best_practice/tilearn.llm/quant/five_minutes_demo/download_pretrained_model.sh
@@ -0,0 +1,4 @@
+# baichuan2 13b
+wget "https://tione-public-cos-1308945662.cos.ap-shanghai.myqcloud.com/models/Baichuan2-13B-Base.tar" -O Baichuan2-13B-Base.tar
+tar -xvf Baichuan2-13B-Base.tar
+echo "The tilearn pretrained model has been downloaded"
--- tikit_best_practice/tilearn.llm/quant/five_minutes_demo/test_baichuan_ppl.py
+++ tikit_best_practice/tilearn.llm/quant/five_minutes_demo/test_baichuan_ppl.py
@@ -7,7 +7,7 @@ from process_data import eval_ppl, get_loaders
 
 parser = argparse.ArgumentParser()
 ## TODO need to change cfs path
-parser.add_argument('--model_name_or_path', type=str, default='/mnt/data/tilearn_demo/Baichuan2-13B-Base',help='HF model')    # Huggingface model name
+parser.add_argument('--model_name_or_path', type=str, default='./Baichuan2-13B-Base',help='HF model')    # Huggingface model name
 parser.add_argument('--dataset', type=str, default='./dataset/wikitext', help='Path to load the dataset.')
 parser.add_argument('--seed', type=int, default=0, help='Seed for sampling the calibration data.')
 parser.add_argument('--nsamples', type=int, default=128, help='Number of calibration samples.')
